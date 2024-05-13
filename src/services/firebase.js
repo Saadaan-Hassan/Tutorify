54abc9df5c6b19b656bc33b5c0ef6eb60af602d3
Saadaan Hassan <webmaker9d@gmail.com>
2024-05-13 18:10:48 +0500

    Update the Account Screen

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -34,4 +35,7 @@ const auth = initializeAuth(app, {
 // Firestore
 const db = getFirestore(app);
 
-export { auth, db };
+// Firebase Storage
+const storage = getStorage(app);
+
+export { auth, db, storage };

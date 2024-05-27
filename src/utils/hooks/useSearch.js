b9c02f8bca24814b1006db258db47c0f0456059b
Saadaan Hassan <webmaker9d@gmail.com>
2024-05-27 17:40:29 +0500

    Add filter option on the Tutors Screen

--- src/utils/hooks/useSearch.js
+++ src/utils/hooks/useSearch.js
@@ -1,35 +1,32 @@
 import { useState, useEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { set } from "firebase/database";
+import { useUser } from "../context/UserContext";
 
 const useSearch = (users) => {
+	const { user } = useUser();
 	const navigation = useNavigation();
 	const route = useRoute();
 
 	const [searchQuery, setSearchQuery] = useState(
-		route.params?.searchQuery || ""
+		route?.params?.searchQuery || ""
 	);
 	const [searchedUsers, setSearchedUsers] = useState(users);
 
 	useEffect(() => {
-		if (route.name !== "Tutors") {
-			// No filtering when route is not Tutors
-			return;
-		}
-
 		const filteredUsers = users.filter((user) =>
 			user.subjects.some((subject) =>
 				subject.toLowerCase().includes(searchQuery.toLowerCase())
 			)
 		);
-
 		setSearchedUsers(filteredUsers);
 	}, [searchQuery, route.name, users]);
 
 	const handleSearchButtonPress = (searchQuery) => {
-		if (route.name !== "Tutors") {
+		if (route.name !== "Tutors" || route.name !== "Students") {
 			// Navigate to Tutors screen and pass search query as parameter
-			navigation.navigate("Tutors", { searchQuery });
+			navigation.navigate(user.role === "Teacher" ? "Students" : "Tutors", {
+				searchQuery: searchQuery.trim(),
+			});
 			setSearchQuery("");
 		}
 	};

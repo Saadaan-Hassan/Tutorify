0c56932fcabe89cf7a81cade47fe294135d3d694
Saadaan Hassan <webmaker9d@gmail.com>
2024-07-02 09:46:59 +0500

    Remove Typescript, add process.env. variables in firbase for eas secrets

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -16,12 +16,13 @@ import {
 
 // Firebase configuration
 const firebaseConfig = {
-	apiKey: FIREBASE_API_KEY,
-	authDomain: FIREBASE_AUTH_DOMAIN,
-	projectId: FIREBASE_PROJECT_ID,
-	storageBucket: FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-	appId: FIREBASE_APP_ID,
+	apiKey: FIREBASE_API_KEY || process.env.FIREBASE_API_KEY,
+	authDomain: FIREBASE_AUTH_DOMAIN || process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: FIREBASE_PROJECT_ID || process.env.FIREBASE_PROJECT_ID,
+	storageBucket: FIREBASE_STORAGE_BUCKET || process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId:
+		FIREBASE_MESSAGING_SENDER_ID || process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: FIREBASE_APP_ID || process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
